9168c9e1523eba5272ba6f5b1f63f4d5e36d3c94
jn-97 <kjo5395@naver.com>
2023-03-21 16:28:02 +0900

    JSX

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,12 @@
 function App() {
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+        </div>
+    );
 }
-
+/* 
+자바스크립트 안에 html 코드를 가지고 있음
+JSX code = 자바스크립트 XML 을 의미함
+*/
 export default App;
