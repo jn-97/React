8b0c4cfbdea4ffdf3e1fd03c2dfafff62f910393
jn-97 <kjo5395@gmail.com>
2023-03-21 19:06:38 +0900

    컴포넌트를 여러 컴포넌트로 분할하기

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,20 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 /* 
 파일 이름은 무조건 대문자로 시작 (소문자로 시작하는 요소는 내장된 html 요소이기 때문이다.)
 단어와 단어를 연결하는 단어 역시 대문자로 작성
 */
 function ExpenseItem(props) { 
-  const month = props.date.toLocaleString('ko-kr', { month: 'long' });
-  const day = props.date.toLocaleString('ko-kr', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>   
