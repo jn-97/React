f00ba9935079f80e4b391655873b28eb3a52bf00
jn-97 <kjo5395@gmail.com>
2023-03-21 18:50:21 +0900

    컴포넌트 분리하기

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -4,9 +4,17 @@ import './ExpenseItem.css';
 단어와 단어를 연결하는 단어 역시 대문자로 작성
 */
 function ExpenseItem(props) { 
+  const month = props.date.toLocaleString('ko-kr', { month: 'long' });
+  const day = props.date.toLocaleString('ko-kr', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>   
